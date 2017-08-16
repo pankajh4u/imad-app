d2a551242732d023692e0d1926324d92cb823cfd
pankajh4u <30925938+pankajh4u@users.noreply.github.com>
2017-08-16 14:23:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.innerHTML = 'New Value';
 
 //Move the image
 var img = document.getElementById('Madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
+    var interval = intInterval(moveRight, 100);
     img.style.marginleft = '200px';
 };
