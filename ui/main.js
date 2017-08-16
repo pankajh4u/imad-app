a02e47c6860328dc8de4e10e8769a2b5b73d8b21
pankajh4u <30925938+pankajh4u@users.noreply.github.com>
2017-08-16 14:14:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text'
 element.innerHTML = 'New Value';
 
 //Move the image
-var img = document.getElementById('img');
+var img = document.getElementById('Madi');
 img.onclick = function () {
     img.style.margileft = '100px';
 };
