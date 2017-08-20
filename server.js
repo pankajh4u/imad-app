0b8cd2467c0a0f79f76df87571af514c2d2bf0c5
pankajh4u <30925938+pankajh4u@users.noreply.github.com>
2017-08-20 12:22:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,9 +101,21 @@ app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
     });
-app.get('/:articleName', function (req, res) {
+    
+app.get('/articles/:articleName', function (req, res) {
     var articleName = req.params.articlename;
-  res.send(createTemplate(articleOne));
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    }); 
 });
 
 app.get('/ui/style.css', function (req, res) {
